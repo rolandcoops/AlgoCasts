6bd79d8b971db8879e1c7c22c521c69bbaad6a86
rolandcoops <rolandcoops@gmail.com>
2019-11-07 13:50:02 +0100

    Finalize (events & sorting)

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor () {
+    this._events = {}
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this._events[eventName]) this._events[eventName] = []
+    
+    this._events[eventName].push(callback)
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (!this._events[eventName]) return false
+
+    for (let callback of this._events[eventName]) callback()
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this._events[eventName] = null
+  }
 }
 
 module.exports = Events;
--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,103 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+/**
+ * Helpers
+ */
 
+// clone an array, or return an empty array for invalid values
+const cloneArr = (arr) =>
+  Array.isArray(arr) ? [...arr] : []
+
+// swap two indeces in array in-place
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]]
+}
+
+/**
+ * Exercises
+ */
+
+function bubbleSort(arr) {
+  let res = cloneArr(arr)
+
+  // end cursor
+  for (let end = res.length - 1; end >= 0; end--) {
+    // scan cursor
+    for (let i = 0; i < end; i++) {
+      if (res[i] > res[i + 1]) swap(res, i, i + 1)
+    }
+  }
+  
+  return res
 }
 
 function selectionSort(arr) {
+  let res = cloneArr(arr)
 
+  // current cursor
+  for (let cur = 0; cur < res.length - 1; cur++) {
+    // index of minimum value, from current position
+    let min = cur
+    // scan cursor, look for any smaller value in rest of array
+    for (let i = cur; i < res.length; i++) {
+      if (res[i] < res[min]) min = i
+    }
+    // swap if nessecary
+    if (min !== cur) swap(res, cur, min)
+  }
+
+  return res
 }
 
-function mergeSort(arr) {
+const mergeSort = (arr) => sort([...arr])
 
+function sort (arr) {
+  if (arr.length <= 1) return arr
+
+  const mid = Math.floor(arr.length / 2)
+  const left = sort(arr.slice(0, mid))
+  const right = sort(arr.slice(mid))
+
+  return merge(left, right)
 }
 
 function merge(left, right) {
+  let l = 0, r = 0
+  let res = []
 
+  // fastest: https://jsperf.com/merge-tests-123456
+  
+  // if first element on left array is less than first of right,
+  // shift left and push in results. Else, shift right and push in results.
+  while (l < left.length && r < right.length) {
+    res.push(left[l] < right[r] ? left[l++] : right[r++])
+  }
+  // take everything from the array that still has values and push in results.
+  while (l < left.length) res.push(left[l++])
+  while (r < right.length) res.push(right[r++])
+
+
+  // // From Algo course (NOTE: optimized for aux array, which is the fastest)
+  // for (let l = 0, r = 0; l < left.length || r < right.length;) {
+  //   // right half exhausted, push left and increment
+  //   if (r > right.length)     res.push(left[l++])
+  //   // left half exhausted, push right and increment
+  //   else if (l > left.length) res.push(right[r++])
+  //   // both halves still have values, push lowest and increment
+  //   else                      res.push(left[l] < right[r] ? left[l++] : right[r++])
+  // }
+  
+  // // SLOWER, but also works...
+  // // if first element on left array is less than first of right,
+  // // shift left and push in results. Else, shift right and push in results.
+  // while (l < left.length && r < right.length) {
+  //   res.push(left[l] < right[r] ? left[l++] : right[r++])
+  // }
+  // // take everything from the array that still has values and push in results.
+  // res.push(...left.slice(l), ...right.slice(r))
+
+  return res
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
